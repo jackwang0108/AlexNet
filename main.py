52f05b1be7d49c9ca50e8416b823348196059fc1
JackWang <jack4shihong@gmail.com>
2022-05-09 18:24:14 +0800

    finished v2 init

--- main.py
+++ main.py
@@ -1,137 +1,314 @@
-import datetime
+"""
+Notes:
+    心得:
+        1. dataset定义处，如果要用torchvision，最好转为Image, 不然容易出错
+        2. 网络定义处，最好写一个warmup来获得分类层这类和输入图像大小有关的数据
+"""
 
-import tqdm
-import numpy as np
+# Standard Library
+import re
+import time
+import logging
+import argparse
+import platform
+import datetime
 from typing import *
+from pathlib import Path
+
+# Third-Party Library
+import numpy as np
 from colorama import Fore, Style, init
 
+# My Library
+from network import AlexNet
+from datasets import MultiDataset
+from helper import ProjectPath, DatasetPath
+from helper import ClassificationEvaluator, ClassLabelLookuper
+from helper import visualize_np, visualize_pil, visualize_plt
+
+# Torch Library
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.utils.data as data
 from torch.utils.tensorboard import SummaryWriter
+import torchvision.transforms as T
 
-from dataset import Cifar10, Cifar100
-from pathconfig import Path, PathConfig
-from network import CifarAlexNet, AlexNet
 
 init(autoreset=True)
 
-class CifarTrainer:
-    available_device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
-    def __init__(self, network: Union[AlexNet, CifarAlexNet]):
-        self.dtype = network.dtype
-        self.device = self.available_device
-        self.network = network.to(device=self.device)
 
-        self.optim = optim.SGD(params=self.network.parameters(), lr=1e-3, weight_decay=5e-4, momentum=0.9)
-        self.loss_function = nn.CrossEntropyLoss()
+class Trainer:
+    start_time: str = datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S")
+    avaliable_device: torch.device = torch.device(
+        "cuda:0" if torch.cuda.is_available() else "cpu:0")
+    default_dtype: torch.dtype = torch.float
 
-        self.train_loader = data.DataLoader(c := Cifar100(split="train"), batch_size=128, shuffle=True, num_workers=1)
-        self.validation_loader = data.DataLoader(Cifar100(split="val"), batch_size=128, shuffle=False, num_workers=1)
-        self.test_loader = data.DataLoader(Cifar100(split="test"), batch_size=128, shuffle=False, num_workers=1)
+    num_worker: int = 0 if platform.system() == "Windows" else 2
 
-        writer_path, self.checkpoint_path = self.decide_path(c)
-        self.writer = SummaryWriter(log_dir=writer_path)
+    train_T = T.Compose([
+        T.Resize(size=(256, 256)),
+        T.RandomCrop(size=(224, 224)),
+        T.RandomHorizontalFlip(),
+        T.ColorJitter(),
+        T.ToTensor(),
+        T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+
+    def __init__(
+        self, network: AlexNet, dataset: str,
+        log: Optional[bool] = True,
+        dry_run: Optional[bool] = True,
+        log_loss_step: Optional[int] = None,
+        log_confusion_epoch: Optional[int] = None
+    ) -> None:
+
+        # save param
+        self.log = log
+        self.dataset: str = dataset
+        self.dry_run: bool = dry_run
+        self.log_loss_step: Union[None, int] = log_loss_step
+        self.log_confusion_epoch: Union[None, int] = log_confusion_epoch
+        self.network: AlexNet = network.to(
+            self.avaliable_device, non_blocking=True)
+
+        # make file suffix
+        suffix = self.dataset + "/" + self.network.__class__.__name__
+
+        # dataset
+        self.train_ds = MultiDataset(dataset=self.dataset, split="train").set_transform(
+            self.train_T
+        )
+        self.val_ds = MultiDataset(dataset=self.dataset, split="val").set_transform(
+            self.train_T
+        )
+
+        # log
+        self.logger = logging.getLogger()
+        self.logger.setLevel(logging.INFO)
+        fmt = logging.Formatter(fmt="%(message)s", datefmt="")
+
+        terminal = logging.StreamHandler()
+        terminal.setLevel(logging.INFO)
+        terminal.setFormatter(fmt=fmt)
+        self.logger.addHandler(terminal)
+
+        if log:
+            self.log_path = ProjectPath.log / suffix / f"{self.start_time}.log"
+            self.log_path.parent.mkdir(parents=True, exist_ok=True)
+            self.logger.info(f"{Fore.YELLOW}Save log to {self.log_path.relative_to(ProjectPath.base)}")
+            file = logging.FileHandler(str(self.log_path))
+            file.setLevel(logging.INFO)
+            file.setFormatter(fmt)
+            self.logger.addHandler(file)
+
+        # Tensorboard
+        if not dry_run:
+            writer_path = ProjectPath.runs / suffix / self.start_time
+            self.logger.info(
+                f"{Fore.YELLOW}Training curves can be found in {writer_path.relative_to(ProjectPath.base)}")
+            self.writer = SummaryWriter(log_dir=writer_path)
+
+        # checkpoints
+        if not dry_run:
+            self.checkpoint_path = ProjectPath.checkpoints / suffix / self.start_time / "best.pt"
+            self.logger.info(
+                f"{Fore.YELLOW}Save checkpoint to {self.checkpoint_path.relative_to(ProjectPath.base)}")
+            self.checkpoint_path.parent.mkdir(parents=True)
 
     def __del__(self):
-        self.writer.close()
+        if not self.dry_run:
+            self.writer.close()
 
-    @staticmethod
-    def decide_path(dataset: data.Dataset) -> List[Path]:
-        start_time = str(datetime.datetime.now())
-        writer_path = PathConfig.runs / dataset.__name__ / start_time
-        pt_path = PathConfig.check_point / dataset.__name__ / start_time / "best.pt"
-        writer_path.mkdir(parents=True)
-        pt_path.parent.mkdir(parents=True)
-        return writer_path, pt_path
+        if self.log:
+            self.logger.info(f"{Fore.MAGENTA}Shutdown at {datetime.datetime.now()}, waiting...")
+        else:
+            print(f"{Fore.MAGENTA}Shutdown at {datetime.datetime.now()}, waiting...")
+        # shutdown logging and flush buffer
+        # wait for write to file
+        time.sleep(3)
+        logging.shutdown()
 
-    def save_check_point(self):
-        torch.save(self.network.state_dict(), f=self.checkpoint_path)
+        # clean logs
+        if self.log:
+            with self.log_path.open(mode="r+") as f:
+                lines = f.readlines()
+                for i in range(len(lines)):
+                    lines[i] = re.sub(r"..\d\dm", "", lines[i])
+                f.seek(0)
+                f.write("".join(lines[:-1]))
 
-    def adjust_lr(self):
-        self.optim.param_groups[0]["lr"] /= 10
+    def modern_train(
+        self, 
+        lr: Optional[float] = 1e-3,
+        n_epoch: Optional[int] = 200,
+        early_stop: Optional[int] = 30,
+        message: Optional[str] = None
+    ) -> AlexNet:
+        # log training digest
+        self.logger.info(f"Training Digest: {message}")
+        self.logger.info(f"AlexNet training with modern setup")
+        self.logger.info(f"lr: {lr}")
+        self.logger.info(f"e_poech: {n_epoch}")
+        self.logger.info(f"early_stop: {early_stop}")
+        self.logger.info(f"datasets: {self.train_ds.dataset}")
+        
 
-    def train(self, n_epoch: int = 3000, early_stop: int = 100):
+        # detect anomaly
+        torch.autograd.set_detect_anomaly(True)
+
+        # train widgets
+        train_evaluator = ClassificationEvaluator(dataset=self.train_ds.dataset)
+        val_evaluator = ClassificationEvaluator(dataset=self.val_ds.dataset)
+        train_loader = data.DataLoader(
+            self.train_ds, batch_size=128, shuffle=True, num_workers=self.num_worker,
+            pin_memory=True
+        )
+        val_loader = data.DataLoader(
+            self.val_ds, batch_size=128, shuffle=False, num_workers=self.num_worker,
+            pin_memory=True
+        )
+        loss_func = nn.CrossEntropyLoss()
+        optimizer = optim.Adam(params=self.network.parameters())
+        optimizer.zero_grad()
+
+        # constant
+        max_top1: float = 0
+        early_stop_cnt: int = 0
+        ne_digits: int = len(str(n_epoch))
+        es_digits: int = len(str(early_stop))
+
+        # typing
         x: torch.Tensor
         y: torch.Tensor
-        y_pred: torch.Tensor
         loss: torch.Tensor
 
-        max_acc = 0
-        ealry_stop_cnt = 0
-        before_stopping = 3
-        for epoch in (tt := tqdm.trange(n_epoch)):
+        # train network
+        for epoch in range(n_epoch):
+            # setup evaluator
+            train_evaluator.new_epoch()
+            val_evaluator.new_epoch()
+
             # train
             self.network.train()
-            for step, (x, y) in enumerate(self.train_loader):
-                x, y = x.to(device=self.device, dtype=self.dtype), y.to(device=self.device, dtype=self.dtype)
+            for step, (x, y) in enumerate(train_loader):
+                x = x.to(device=self.avaliable_device, dtype=self.default_dtype, non_blocking=True)
+                y = y.to(device=self.avaliable_device, dtype=torch.long)
+
+                # inference
                 y_pred = self.network(x)
-                train_loss = self.loss_function(y_pred.float(), y.long())
-                train_loss.backward()
-                self.optim.step()
-                self.network.zero_grad()
+
+                # gradient descent
+                loss = loss_func(y_pred, y)
+                loss.backward()
+                optimizer.step()
+                optimizer.zero_grad()
 
                 # log
-                self.writer.add_scalar(tag="loss/train", scalar_value=train_loss.item(),
-                                       global_step=epoch * len(self.train_loader) + step)
+                if self.log and (self.log_loss_step is not None and step % self.log_loss_step == 0):
+                    self.logger.info(f"{Fore.GREEN}Dataset: {self.dataset}, Epoch: [{epoch:>{ne_digits}d}|{n_epoch}], step: {step}, loss: {loss:>.5f}")
+                if not self.dry_run:
+                    if step % self.log_loss_step == 0:
+                        self.writer.add_scalar(tag="train-loss", scalar_value=loss.cpu().item(), global_step=step + len(train_loader) * epoch)
+                train_evaluator.record(y_pred=y_pred, y=y)
 
             # val
-            val_acc = 0
-            total_sum = 0
             self.network.eval()
             with torch.no_grad():
-                for step, (x, y) in enumerate(self.validation_loader):
-                    x, y = x.to(device=self.device, dtype=self.dtype), y.to(device=self.device, dtype=self.dtype)
+                for step, (x, y) in enumerate(val_loader):
+                    x = x.to(device=self.avaliable_device, dtype=self.default_dtype, non_blocking=True)
+                    y = y.to(device=self.avaliable_device, dtype=torch.long)
+
+                    # inference
                     y_pred = self.network(x)
-                    val_loss = self.loss_function(y_pred.float(), y.long())
 
-                    # log loss
-                    self.writer.add_scalar(tag="loss/val", scalar_value=val_loss.item(),
-                                           global_step=epoch * len(self.validation_loader) + step)
-                    val_acc += torch.sum(y_pred.argmax(dim=1) == y)
-                    total_sum += len(y)
-            self.writer.add_scalar(tag="acc/val", scalar_value=(val_acc := (val_acc / total_sum)).item(),
-                                   global_step=epoch)
-
-            # test
-            test_acc = 0
-            total_sum = 0
-            with torch.no_grad():
-                for step, (x, y) in enumerate(self.test_loader):
-                    x, y = x.to(device=self.device, dtype=self.dtype), y.to(device=self.device, dtype=self.dtype)
-                    y_pred = self.network(x)
-                    test_acc += torch.sum(y_pred.argmax(dim=1) == y)
-                    total_sum += len(y)
-            self.writer.add_scalar(tag="acc/test", scalar_value=(test_acc := (test_acc / total_sum)).item(),
-                                   global_step=epoch)
-            tt.write(
-                f"Epoch [{epoch:>5d}|{n_epoch:>5d}], train loss: {train_loss:>7.6f}, val loss: {val_loss:>7.6f}, "
-                f"val acc: {Fore.GREEN}{val_acc*100:>4.2f}%{Style.RESET_ALL}, max acc: {max_acc*100:>4.2f}%, test acc: {test_acc*100:>4.2f}%, "
-                f"early stop: [{Fore.BLUE}{ealry_stop_cnt:>5d}{Style.RESET_ALL}|{early_stop:>5d}]"
-            )
-
-            if val_acc > max_acc:
-                max_acc = val_acc
-                ealry_stop_cnt = 0
-                self.save_check_point()
+                    # log
+                    val_evaluator.record(y_pred=y_pred, y=y)
+            
+            # early stop
+            new_acc = val_evaluator.acc
+            if max_top1 <= new_acc[0]:
+                early_stop_cnt = 0
+                max_top1 = new_acc[0]
+                self.logger.info(
+                    f"{Fore.BLUE}Dataset: {self.dataset}, Epoch: [{epoch:>{ne_digits}}|{n_epoch}], "\
+                    f"new top1 Acc: {new_acc[0]:>.5f}, top5 Acc:{new_acc[1]:>.5f}"
+                )
+                if not self.dry_run:
+                    torch.save(self.network.state_dict(), self.checkpoint_path)
+                    self.logger.info(
+                        f"{Fore.MAGENTA}Dataset: {self.dataset}, Epoch: [{epoch:>{ne_digits}}|{n_epoch}], "\
+                        f"save checkpoint to {self.checkpoint_path.relative_to(ProjectPath.base)}"
+                    )
             else:
-                ealry_stop_cnt += 1
+                early_stop_cnt += 1
+                self.logger.info(
+                    f"{Fore.BLUE}Dataset: {self.dataset}, Epoch: [{epoch:>{ne_digits}}|{n_epoch}], "\
+                    f"top1 Acc: [{new_acc[0]:>5f}|{max_top1:>.5f}], top5 Acc: [{new_acc[1]:>5f}] early_stop_cnt: [{early_stop_cnt:>{es_digits}d}|{early_stop}]"
+                )
 
-            # adjust lr as said in paper
-            # if ealry_stop_cnt > 20 and before_stopping > 0:
-            #     before_stopping -= 1
-            #     early_stop_cn = 0
-            #     self.adjust_lr()
+            # tensorboard
+            if not self.dry_run:
+                self.writer.add_scalars(
+                    main_tag=f"Train Accuracy",
+                    tag_scalar_dict={
+                        "mAcc-top1": new_acc[0],
+                        "mAcc-top5": new_acc[1],
+                        "max-mAcc-top1": max_top1
+                    },
+                    global_step=epoch
+                )
+            
+            # print confusion matrix
+            if self.log_confusion_epoch is not None and epoch % self.log_confusion_epoch == 0:
+                table = val_evaluator.get_confusion(top=5, title=f"Top 5 Confusion Matrix of dataset {self.dataset}")
+                if self.log:
+                    self.logger.info(str(table))
+                else:
+                    print(table)
 
-            if ealry_stop_cnt > early_stop:
-                tt.write(f"{Fore.RED}Early Stopped!")
-                break
-        return self
+        return self.network
 
+
+    def paper_train(
+        self, 
+    ) -> AlexNet:
+        pass
+
+def parse_arg() -> argparse.Namespace:
+    def green(s): return f"{Fore.GREEN}{s}{Style.RESET_ALL}"
+    def yellow(s): return f"{Fore.YELLOW}{s}{Style.RESET_ALL}"
+    def blue(s): return f"{Fore.BLUE}{Style.BRIGHT}{s}{Style.RESET_ALL}"
+
+    parser = argparse.ArgumentParser(description=blue("iCaRL Pytorch Implementation training util by Shihong Wang (Jack3Shihong@gmail.com)"))
+    parser.add_argument("-v", "--version", action="version", version="%(prog)s v2.0, fixed training bugs, but there's still GPU memory leak problem")
+    parser.add_argument("-d", "--dry_run", dest="dry_run", default=False, action="store_true", help=green("If run without saving tensorboard amd network params to runs and checkpoints"))
+    parser.add_argument("-l", "--log", dest="log", default=False, action="store_true", help=green("If save terminal output to log"))
+    parser.add_argument("-ne", "--n_epoch", dest="n_epoch", type=int, default=250, help=yellow("Set maximum training epoch of each task"))
+    parser.add_argument("-es", "--early_stop", dest="early_stop", type=int, default=40, help=yellow("Set maximum early stop epoch counts"))
+    parser.add_argument("-ds", "--dataset", dest="dataset", type=str, default="Cifar10", help=blue("Set training datasets"))
+    parser.add_argument("-m", "--message", dest="message", type=str, default=f"", help=blue("Training digest"))
+    return parser.parse_args()
 
 
 if __name__ == "__main__":
-    ca = CifarAlexNet(in_size=1024, predict_class=256)
-    ct = CifarTrainer(network=ca).train()
+    # get arg
+    args = get_args()
+    
+    # Attention: Parameters
+    log: bool = args.log
+    n_epoch: int = args.n_epoch
+    dry_run: bool = args.dry_run
+    early_stop: int = args.early_stop
+    messgae: str = args.message
+    dataset: str = args.dataset.capitalize()
+
+    assert dataset in (s:=["Cifar10", "Cifar100", "PascalVOC2012"]), f"{Fore.RED}Invalid Datasets, please select in {s}"
+
+    network = AlexNet(predict_class=len(ClassLabelLookuper(datasets=dataset).cls))
+    network = Trainer(
+        network=network, dataset=dataset, log=log, dry_run=dry_run, 
+        log_loss_step=100,
+        log_confusion_epoch=1
+    ).modern_train(message="DEBUG RUN")
